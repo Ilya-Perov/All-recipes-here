e58bec8a70b13b8744103a550d9de4dac20e2649
Ilya-Petrov <petrovilya2013@gmail.com>
2025-12-16 21:49:57 +0300

    added files

--- frontend/webpage/src/api/api.ts
+++ frontend/webpage/src/api/api.ts
@@ -1,4 +1,4 @@
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = process.env.REACT_APP_API_URL as string;
 
 export const createRecipe = async (payload: any) => {
   const response = await fetch(API_URL, {
